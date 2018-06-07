40836cbb704d9911c124719da55ea5ce2b4db857
Julian Bragazzi <julian.bragazzi@despegar.com>
2018-06-07 15:33:54 -0300

    Login Angular!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  formModel = { user: '', pass: '' };
+
+  constructor(
+    public http: HttpClient
+  ) { }
+
+  post() {
+    let json = JSON.stringify({ name: 'julian' });
+
+    //El backend recogerá un parametro json
+    // let params = "json=" + json;
+    const body = new HttpParams()
+      .set(`name`, 'aaaaaaaaaaa');
+    const lala = 'foo';
+    //Establecemos cabeceras
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post('http://localhost/index.php', body.toString(), { headers: headers })
+      .subscribe(x => console.log(x));
+  }
+
+
+  onSubmit() {
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+    const body = new HttpParams()
+      .set('user', this.formModel.user)
+      .set('pass', this.formModel.pass);
+    return this.http.post('http://localhost/index.php', body.toString(), { headers: headers })
+      .subscribe(x => console.log(x));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
