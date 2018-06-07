aa60e15e23094dc1d438c9ae73950bc8756ff8ae
Julian Bragazzi <julian.bragazzi@despegar.com>
2018-06-07 12:01:05 -0300

    Arrancamos en limpio

--- core/login/js/funciones.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#btnSubmit').on('click',function(){
-        $('#logForm').submit();
-    })
-
-});
--- views/index.php
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-
-
-
-<h1><?php echo $welcome; ?></h1>
