5688dba46fe79e52e851c38126134aaadd369d85
Gonzalo Matias Peredo <gonzalo.peredo@educacion.local>
2018-06-12 13:28:33 -0300

    Agrego correciones a los script del seed

--- scripts/seed/seedConsorcio.sql
+++ scripts/seed/seedConsorcio.sql
@@ -1,11 +1,12 @@
-insert into consorcio (id_consorcio, nombre, cuit, calle, altura, telefono, superficie, coordenadaLatitud, coordenadaLongitud) 
- values  (1, 'Brainlounge', '62-0553006', 'Lillian Terrace', '89', '4218939667', 	522, -23.5441298, -46.3660676),
-		 (2, 'Twinte', '98-3560731', 'Mockingbird Way', '46778', '2012084954', 		497, 50.24987, -117.80226),
-		 (3, 'Skinder', '08-1950452', 'Stoughton Lane', '6674', '8036920348', 		460, -10.0662772, 38.9276622),
-		 (4, 'Dabjam', '22-4465641', 'Myrtle Way', '95693', '8684815073', 			496, -8.2919428, 123.2254725),
-		 (5, 'Gabspot', '82-6677993', 'Mandrake Street', '84', '2624567331', 		506, 44.596795, 20.1242053),
-		 (6, 'Buzzbean', '58-6465474', 'Buena Vista Alley', '7', '9419825578', 		493, 1.0452874, 33.7992536),
-		 (7, 'Fiveclub', '50-8145880', 'Boyd Point', '990', '3562424306', 			462, 9.3887516, -67.0613528),
-		 (8, 'Kazio', '64-6149962', 'Mendota Center', '140', '4714435424', 			480, 47.9035673, 1.9638715),
-		 (9, 'Aivee', '51-0540923', 'Glacier Hill Avenue', '28', '5783617172', 		480, 36.3566484, 43.1640004),
-		 (10, 'Yakitri', '01-6394543', 'Sunbrook Hill', '30', '2769027591', 		499, -37.1534439, -56.8913573);
+insert into consorcio (id_consorcio, nombre, cuit, calle, altura, telefono, superficie, coordenadaLatitud, coordenadaLongitud, id_barrio) 
+ values  (1, 'Brainlounge', '62-0553006', 'Lillian Terrace', 		'89', 		'4218939667', 		522,	 -23.5441298, 	-46.3660676,	1),
+		 (2, 'Twinte', 		'98-3560731', 'Mockingbird Way', 		'46778', 	'2012084954', 		497, 	50.24987, 		-117.80226,		3),
+		 (3, 'Skinder', 	'08-1950452', 'Stoughton Lane', 		'6674', 	'8036920348', 		460, 	-10.0662772, 	38.9276622,		5),
+		 (4, 'Dabjam', 		'22-4465641', 'Myrtle Way', 			'95693', 	'8684815073', 		496, 	-8.2919428, 	123.2254725,	13),
+		 (5, 'Gabspot', 	'82-6677993', 'Mandrake Street', 		'84', 		'2624567331', 		506, 	44.596795, 		20.1242053,		7),
+		 (6, 'Buzzbean', 	'58-6465474', 'Buena Vista Alley', 		'7', 		'9419825578', 		493, 	1.0452874, 		33.7992536,		2),
+		 (7, 'Fiveclub', 	'50-8145880', 'Boyd Point', 			'990', 		'3562424306', 		462, 	9.3887516, 		-67.0613528,	20),
+		 (8, 'Kazio', 		'64-6149962', 'Mendota Center', 		'140', 		'4714435424', 		480, 	47.9035673,		1.9638715,		1),
+		 (9, 'Aivee', 		'51-0540923', 'Glacier Hill Avenue', 	'28', 		'5783617172', 		480, 	36.3566484, 	43.1640004,		15),
+		 (10,'Yakitri', 	'01-6394543', 'Sunbrook Hill', 			'30', 		'2769027591', 		499, 	-37.1534439, 	-56.8913573,	9);
+
