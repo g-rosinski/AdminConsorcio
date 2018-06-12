db904ff8be4859aff726f080759445f43a0ae883
Julian Bragazzi <julian.bragachi@gmail.com>
2018-06-11 21:40:38 -0300

    DELETE VIOLENTO SOY INIMPUTABLE

--- scripts/seed/seedComuna.sql
+++
@@ -1,4 +0,0 @@
-insert into iani.comuna(id_comuna,descripcion)
-values	(1,	'Comuna 1'),
-		(2,	'Comuna 2'),
-		(3,	'Comuna 3');
--- scripts/seed/seedRol.sql
+++
@@ -1,4 +0,0 @@
-insert into rol (id_rol,descripcion)
-values	( 1, 'Administrador'),
-		( 2, 'Operador'		),
-        ( 3, 'Propietario'	);
