c7c78c35c6a23aa4f95b78db8bf53d442ef850cd
g-rosinski <gonzarosinski@gmail.com>
2018-07-17 00:22:55 -0300

    Agrego comentario sobre array que devuelve para pdf

--- src/server/api/actions/expensa/traerDatosDeExpensa.php
+++ src/server/api/actions/expensa/traerDatosDeExpensa.php
@@ -24,3 +24,31 @@ function traerDatosDeExpensa()
 
     return json_encode($arrayDetalleExpensa);
 }
+// Detalle que devuelve:
+//
+/* Array(
+	'expensa' => Array(
+			'total'=> Total de la expensa seleccionada
+			'impteExtraordinaria'=> Subtotal calculado de los gastos del mes
+			'mora' => Importe de mora por expensas vencidas
+			'cuotaAnual'=> nro de cuota de la expensa
+			'vencimiento'=> fecha de vencimiento yyyy-mm-dd
+			'idGastoMensual'=> id de la liquidacion, no va al pdf
+	)
+	'gastos' => Array(
+        'rubros'=> Array(
+            0 => Array(
+                '<nombreRubro>' => Array(
+                    '<motivoDelGasto>'=>nombre
+                    'totalGasto'=> total gastado en esto
+                    'detalle'=> Array(
+                        'proveedor' => nombre de proveedor
+                        'importe' => importe del gasto
+                        'descripcion' => mensaje del gasto
+                    )
+                )
+            )
+        )
+    )
+)
+ */
